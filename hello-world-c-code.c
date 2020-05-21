00eac89cb993f9289cb99233ad2ad0178024f583
Larry Cheever <lach1105@gmail.com>
2020-05-21 11:06:12 +0200

    Test usage of \n

--- hello-world-c-code.c
+++ hello-world-c-code.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
  
 int main(void) {
-   printf("Hello: World?")
+   printf("Hello: World?");
    printf("..Bye..");
+   printf(".. Bye .. \n");
+   printf("Hello & again! \n");
    return 0;
 }
